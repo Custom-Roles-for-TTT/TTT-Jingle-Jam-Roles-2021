5b4e6d24ce76949588dc614be58ba59a9a4e694b
Malivil <Malivil@users.noreply.github.com>
2022-03-18 08:23:51 -0400

    Changed to ExtDescription
    And use it first

--- gamemodes/terrortown/entities/entities/ttt_randoman_items/shared.lua
+++ gamemodes/terrortown/entities/entities/ttt_randoman_items/shared.lua
@@ -178,8 +178,8 @@ if SERVER then
                     -- Update randomat description
                     local description = "'" .. longName .. "' is triggered when you buy this."
 
-                    if (event.Description and event.Description ~= "") or (event.AltDescription and event.AltDescription ~= "") then
-                        description = event.Description or event.AltDescription
+                    if (event.ExtDescription and #event.ExtDescription > 0) or (event.Description and #event.Description > 0) then
+                        description = event.ExtDescription or event.Description
 
                         if descriptionName then
                             description = longName .. "\n\n" .. description
